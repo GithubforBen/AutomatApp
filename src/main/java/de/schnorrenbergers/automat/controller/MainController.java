a26b65c713286b1f8d57e97d2188b35d15b9d051
Ben <01bensch@gmail.com>
2025-03-28 16:33:30 +0100

    better imports

--- src/main/java/de/schnorrenbergers/automat/controller/MainController.java
+++ src/main/java/de/schnorrenbergers/automat/controller/MainController.java
@@ -12,7 +12,6 @@ import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import org.json.JSONObject;
 
 import java.io.IOException;
