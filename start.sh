5243fe35a358a79bee47149cdaec8a8d59cfe469
Ben <01bensch@gmail.com>
2026-01-23 17:48:51 +0100

    Improve `start.sh` script: add symlink creation, refine `tmux` commands, and set correct working directory
    Signed-off-by: Ben <01bensch@gmail.com>

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
+ln -s /home/ben/Documents/AutomatApp/start.sh /home/ben/Desktop/start
+cd /home/ben/Documents/AutomatApp
 git pull
 sudo apt install tmux -y
 sudo apt install openjdk-21-jdk -y
 ./mvnw clean install
 tmux new-session -d -s javaBackend
-tmux send-keys -t javaBackend "./mvnw clean install javafx:run"
+tmux send-keys -t javaBackend "cd /home/ben/Documents/AutomatApp" c-m
+tmux send-keys -t javaBackend "./mvnw clean install javafx:run" c-m
